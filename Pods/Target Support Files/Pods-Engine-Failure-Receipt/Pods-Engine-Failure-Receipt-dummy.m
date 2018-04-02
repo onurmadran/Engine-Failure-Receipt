ca242d1129f22f513d9608e03393b22bc010c82a
onurmadran <onurmadran@windowslive.com>
2018-04-02 13:24:54 +0300

    init

---
+++ Pods/Target Support Files/Pods-Engine-Failure-Receipt/Pods-Engine-Failure-Receipt-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Engine_Failure_Receipt : NSObject
+@end
+@implementation PodsDummy_Pods_Engine_Failure_Receipt
+@end
---
+++ Pods/Target Support Files/Pods-Engine-Failure-Receipt/Pods-Engine-Failure-Receipt-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Engine_Failure_ReceiptVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Engine_Failure_ReceiptVersionString[];
+
